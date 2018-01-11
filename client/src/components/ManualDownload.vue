d50a5eaf13109d8594c74c277e7a960f59f26531
RTANC <wongsathon_n@rtanc.ac.th>
2018-01-11 13:36:37 +0700

    add all manual level

--- client/src/components/ManualDownload.vue
+++ client/src/components/ManualDownload.vue
@@ -2,15 +2,15 @@
 <v-container fluid grid-list-xs>
   <v-layout class="pt-5" row wrap>
     <v-flex xs2 offset-xs1>
-      <v-select v-bind:items="year" label="เลือกปีงบประมาณ" class="input-group--focused">
+      <v-select @change="onYearChange" v-bind:items="allYear" label="เลือกปีงบประมาณ" class="input-group--focused">
       </v-select>
     </v-flex>
     <v-flex xs3 offset-xs1>
-      <v-select v-bind:items="institute" label="เลือกสถาบันผู้ประเมิน" class="input-group--focused">
+      <v-select @change="onInstituteChange" v-bind:items="allinstitute" label="เลือกสถาบันผู้ประเมิน" class="input-group--focused">
       </v-select>
     </v-flex>
-    <v-flex xs3 offset-xs1>
-      <v-select v-bind:items="manlvl" label="ระดับของคู่มือ" class="input-group--focused">
+    <v-flex xs4 offset-xs1>
+      <v-select @change="onManLvlChange" v-bind:items="allManLvl" label="ระดับของคู่มือ" class="input-group--focused">
       </v-select>
     </v-flex>
     <v-flex xs5 offset-xs1>
@@ -41,70 +41,115 @@
 </template>
 
 <script>
-var tmp = []
-var nowYear = ((new Date()).getFullYear()) + 543
-for (var i = nowYear - 3; i <= nowYear + 3; i++) {
-  tmp.push({
-    text: '' + i
-  })
-}
-export default {
-  name: 'ManualDownload',
-  data: () => {
-    return {
-      year: tmp,
-      manualFiles: {
-        hasFile: false,
-        files: null
+  var tmp = []
+  var nowYear = ((new Date()).getFullYear()) + 543
+  for (var i = nowYear - 3; i <= nowYear + 3; i++) {
+    tmp.push({
+      text: i,
+      value: i
+    })
+  }
+  export default {
+    name: 'ManualDownload',
+    data: () => {
+      return {
+        allYear: tmp,
+        manualFiles: {
+          hasFile: false,
+          files: null
+        },
+        allinstitute: [{
+          text: 'สำนักงานคณะกรรมการการอุดมศึกษา',
+          value: 1
+        }, {
+          text: 'สภาการพยาบาล',
+          value: 2
+        }, {
+          text: 'สถาบันวิชาการป้องกันประเทศ',
+          value: 3
+        }, {
+          text: 'กรมยุทธศึกษาทหารบก',
+          value: 4
+        }],
+        allManLvl: [{
+          text: 'วิทยาลัยพยาบาลกองทัพบก',
+          value: 1
+        }, {
+          text: 'กองอำนวยการ',
+          value: 2
+        }, {
+          text: 'กองการศึกษา',
+          value: 3
+        }, {
+          text: 'กองการปกครอง',
+          value: 4
+        }, {
+          text: 'แผนกเตรียมการ',
+          value: 5
+        }, {
+          text: 'แผนกสนับสนุน',
+          value: 6
+        }, {
+          text: 'แผนกธุรการ/กำลังพล',
+          value: 7
+        }, {
+          text: 'ภาควิชาความรู้พื้นฐาน',
+          value: 8
+        }, {
+          text: 'ภาควิชาการพยาบาลเบื้องต้น',
+          value: 9
+        }, {
+          text: 'ภาควิชาการพยาบาลอายุรศาสตร์และศัลยศาสตร์',
+          value: 10
+        }, {
+          text: 'ภาควิชาพยาบาลสูติศาสตร์',
+          value: 11
+        }, {
+          text: 'ภาควิชาการพยาบาลกุมารเวชศาสตร์',
+          value: 12
+        }, {
+          text: 'ภาควิชาสุขภาพจิตและการพยาบาลจิตเวชศาสตร์',
+          value: 13
+        }, {
+          text: 'ภาควิชาการพยาบาลอนามัยชุมชน'
+        }]
+      }
+    },
+    methods: {
+      onSearchFile () {
+
       },
-      institute: [{
-        text: 'สำนักงานคณะกรรมการการอุดมศึกษา',
-        value: 1
-      }, {
-        text: 'สภาการพยาบาล',
-        value: 2
-      }],
-      manlvl: [{
-        text: 'วิทยาลัยพยาบาลกองทัพบก',
-        value: 1
-      }, {
-        text: 'กองอำนวยการ',
-        value: 2
-      }, {
-        text: 'กองการศึกษา',
-        value: 3
-      }, {
-        text: 'กองการปกครอง',
-        value: 4
-      }]
-    }
-  },
-  methods: {
-    onSearchFile () {
-      this.$refs.fileInput.click()
+      onYearChange (value) {
+        this.year = value
+      },
+      onInstituteChange (value) {
+        this.institute = value
+      },
+      onManLvlChange (value) {
+        this.manLvl = value
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
--- client/src/components/ManualUpload.vue
+++ client/src/components/ManualUpload.vue
@@ -2,15 +2,15 @@
 <v-container fluid grid-list-xs>
   <v-layout class="pt-5" row wrap>
     <v-flex xs2 offset-xs1>
-      <v-select v-bind:items="year" label="เลือกปีงบประมาณ" class="input-group--focused">
+      <v-select @change="onYearChange" v-bind:items="allYear" label="เลือกปีงบประมาณ" class="input-group--focused">
       </v-select>
     </v-flex>
     <v-flex xs3 offset-xs1>
-      <v-select v-bind:items="institute" label="เลือกสถาบันผู้ประเมิน" class="input-group--focused">
+      <v-select @change="onInstituteChange" v-bind:items="allinstitute" label="เลือกสถาบันผู้ประเมิน" class="input-group--focused">
       </v-select>
     </v-flex>
-    <v-flex xs3 offset-xs1>
-      <v-select v-bind:items="manlvl" label="ระดับของคู่มือ" class="input-group--focused">
+    <v-flex xs4 offset-xs1>
+      <v-select @change="onManLvlChange" v-bind:items="allManLvl" label="ระดับของคู่มือ" class="input-group--focused">
       </v-select>
     </v-flex>
     <v-flex xs5 offset-xs1>
@@ -42,77 +42,125 @@
 </template>
 
 <script>
-var tmp = []
-var nowYear = ((new Date()).getFullYear()) + 543
-for (var i = nowYear - 3; i <= nowYear + 3; i++) {
-  tmp.push({
-    text: '' + i
-  })
-}
-export default {
-  name: 'ManualUpload',
-  data: () => {
-    return {
-      year: tmp,
-      manualFiles: {
-        hasFile: false,
-        files: null
-      },
-      institute: [{
-        text: 'สำนักงานคณะกรรมการการอุดมศึกษา',
-        value: 1
-      }, {
-        text: 'สภาการพยาบาล',
-        value: 2
-      }],
-      manlvl: [{
-        text: 'วิทยาลัยพยาบาลกองทัพบก',
-        value: 1
-      }, {
-        text: 'กองอำนวยการ',
-        value: 2
-      }, {
-        text: 'กองการศึกษา',
-        value: 3
-      }, {
-        text: 'กองการปกครอง',
-        value: 4
-      }]
-    }
-  },
-  methods: {
-    onPickFile () {
-      this.$refs.fileInput.click()
-    },
-    onFilePicked (evt) {
-      if (evt.target.files[0].type !== evt.target.accept) {
-        return alert('ชนิดของไฟล์ต้องเป็น PDF เท่านั้น')
+  var tmp = []
+  var nowYear = ((new Date()).getFullYear()) + 543
+  for (var i = nowYear - 3; i <= nowYear + 3; i++) {
+    tmp.push({
+      text: i,
+      value: i
+    })
+  }
+  export default {
+    name: 'ManualUpload',
+    data: () => {
+      return {
+        year: null,
+        institute: null,
+        manLvl: null,
+        manualFiles: {
+          hasFile: false,
+          files: null
+        },
+        allYear: tmp,
+        allinstitute: [{
+          text: 'สำนักงานคณะกรรมการการอุดมศึกษา',
+          value: 1
+        }, {
+          text: 'สภาการพยาบาล',
+          value: 2
+        }, {
+          text: 'สถาบันวิชาการป้องกันประเทศ',
+          value: 3
+        }, {
+          text: 'กรมยุทธศึกษาทหารบก',
+          value: 4
+        }],
+        allManLvl: [{
+          text: 'วิทยาลัยพยาบาลกองทัพบก',
+          value: 1
+        }, {
+          text: 'กองอำนวยการ',
+          value: 2
+        }, {
+          text: 'กองการศึกษา',
+          value: 3
+        }, {
+          text: 'กองการปกครอง',
+          value: 4
+        }, {
+          text: 'แผนกเตรียมการ',
+          value: 5
+        }, {
+          text: 'แผนกสนับสนุน',
+          value: 6
+        }, {
+          text: 'แผนกธุรการ/กำลังพล',
+          value: 7
+        }, {
+          text: 'ภาควิชาความรู้พื้นฐาน',
+          value: 8
+        }, {
+          text: 'ภาควิชาการพยาบาลเบื้องต้น',
+          value: 9
+        }, {
+          text: 'ภาควิชาการพยาบาลอายุรศาสตร์และศัลยศาสตร์',
+          value: 10
+        }, {
+          text: 'ภาควิชาพยาบาลสูติศาสตร์',
+          value: 11
+        }, {
+          text: 'ภาควิชาการพยาบาลกุมารเวชศาสตร์',
+          value: 12
+        }, {
+          text: 'ภาควิชาสุขภาพจิตและการพยาบาลจิตเวชศาสตร์',
+          value: 13
+        }, {
+          text: 'ภาควิชาการพยาบาลอนามัยชุมชน'
+        }]
       }
-      this.manualFiles.files = evt.target.files
-      this.manualFiles.hasFile = true
+    },
+    methods: {
+      onPickFile () {
+        this.$refs.fileInput.click()
+      },
+      onFilePicked (evt) {
+        if (evt.target.files[0].type !== evt.target.accept) {
+          return alert('ชนิดของไฟล์ต้องเป็น PDF เท่านั้น')
+        }
+        this.manualFiles.files = evt.target.files
+        this.manualFiles.hasFile = true
+      },
+      onYearChange (value) {
+        this.year = value
+      },
+      onInstituteChange (value) {
+        this.institute = value
+      },
+      onManLvlChange (value) {
+        this.manLvl = value
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
