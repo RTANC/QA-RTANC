192e46bc1fa524207230c1aeb2b2108057f00309
RTANC <wongsathon_n@rtanc.ac.th>
2018-02-27 14:44:21 +0700

    add delete doc ref api

--- server/api/routes/docRefs.js
+++ server/api/routes/docRefs.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const sarDocRef = require('./models/sarDocRef')
+const fs = require('fs')
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, 'uploads/DocumentRefs/')
@@ -26,6 +27,8 @@ router.get('/', (req, res, next) => {
         }else{
             res.status(200).send(docs)
         }
+    }).catch(err => {
+        next(err)
     })
 })
 
@@ -39,7 +42,20 @@ router.post('/', upload.single('docRef'), (req, res, next) => {
     }).then(succ => {
         res.status(200).json('upload file success')
     }).catch(err => {
-        res.status(422).json('upload file fail')
+        next(err)
+    })
+})
+
+router.delete('/', multer().array(), (req, res, next) => {
+    sarDocRef.destroy({
+        where: {
+            docRefId: req.body.docRefId
+        }
+    }).then(succ => {
+        fs.unlinkSync('uploads/DocumentRefs/' + req.body.filename)
+        res.status(200).json('Documents deleted')
+    }).catch(err => {
+        next(err)
     })
 })
 
