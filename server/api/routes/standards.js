5566eb8513a244ae856bbfec50c1ef60414a5d01
RTANC <wongsathon_n@rtanc.ac.th>
2018-05-07 21:55:33 +0700

    add duplicate standard api

--- server/api/routes/standards.js
+++ server/api/routes/standards.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const standard = require('./models/standard')
+const indicator = require('./models/indicator')
+const Op = require('sequelize').Op
 
 router.get('/', (req, res, next) => {
     const year = req.query.year
@@ -33,6 +35,63 @@ router.post('/', multer().array(),(req, res, next) => {
         res.send(nRow.dataValues)
     }).catch(err =>{
         next(err)
+    })
+})
+
+router.post('/dup', (req, res, next )=> {
+    standard.hasMany(indicator, {foreignKey: 'standardId'})
+    indicator.belongsTo(standard, {foreignKey: 'standardId'})
+    standard.findAll({
+        where: {
+            year: (req.body.year) - 1,
+            institute: req.body.institute
+        },
+        include: [{
+            model: indicator,
+            where: {
+                standardId: {
+                    [Op.ne]: null
+                }
+            }
+        }]
+    }).then(x => {
+        const std = []
+        for (var i = 0;i < x.length;i++) {
+            std.push({
+                year: x[i].year,
+                institute: x[i].institute,
+                standardNo: x[i].standardNo,
+                standardName: x[i].standardName,
+                standardLvl: x[i].standardLvl
+            })
+        }
+
+        standard.bulkCreate(std).then(y => {
+                standard.findAll({
+                    where: {
+                        year: req.body.year,
+                        institute: req.body.institute
+                    }
+                }).then(z => {
+                    for (var i = 0;i < z.length;i++) {
+                        const ind = []
+                        for(var j = 0;j < x[i].indicators.length;j++) {
+                            ind.push({
+                                standardId: z[i].standardId,
+                                indicatorNo: x[i].indicators[j].indicatorNo,
+                                indicatorName: x[i].indicators[j].indicatorName,
+                                indicatorInfo: x[i].indicators[j].indicatorInfo,
+                                indicatorType: x[i].indicators[j].indicatorType,
+                                indicatorGain: x[i].indicators[j].indicatorGain
+                            })
+                        }
+                        indicator.bulkCreate(ind)
+                    }
+                })
+        }).catch(err => {
+            next(err)
+        })
+
     })
 })
 
